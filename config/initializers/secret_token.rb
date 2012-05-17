5c33aaeb2d7574014c6781433025ba149ad09d0e
Rufo Sanchez <rufo@rufosanchez.com>
2012-05-17 18:50:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitlist::Application.config.secret_token = 'c62e5887bf70487f9da406a4adf49ded9eedfce1f575145e67ad02afb0eec7414a7c93195cf90fa79565d85bd9148214aa9022f927f5aed899998286b7c8bd9f'
